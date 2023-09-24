8b87670c9a8db299f73bddee10fc982192c3562e
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-24 21:37:21 +0530

    feat: create light and dark ThemeToggle component

---
+++ static/theme.js
@@ -0,0 +1,13 @@
+//Prevent FOUC related to theme (light & dark mode)
+
+/* sets class = dark (Use with Tailwind & daisyUI) */
+if (localStorage.getItem('data-theme') === 'dark') {
+	document.documentElement.setAttribute('data-theme', 'dark');
+} else if (
+	!localStorage.getItem('data-theme') &&
+	window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+	document.documentElement.setAttribute('data-theme', 'dark');
+} else {
+	document.documentElement.setAttribute('data-theme', 'light');
+}
