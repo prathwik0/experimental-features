5823bf3229983b11f127a16700240834ef77ecaa
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-09-24 18:28:30 +0530

    feat: got the list items

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,12 @@
 import type { PageLoad } from './$types';
+import { db } from '$lib/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 export const load = (async () => {
-
-	return {};
+	const quiz = collection(db, 'Quiz');
+	const quizSnapshot = await getDocs(quiz);
+	const quizList = quizSnapshot.docs.map((doc) => doc.data());
+	console.log(quizList);
+	const questions = quizList[0].list;
+	return { item: quizList };
 }) satisfies PageLoad;
